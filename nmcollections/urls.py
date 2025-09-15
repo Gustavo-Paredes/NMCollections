478edc7e838e1143cb26f2f65b436a434488bb64
Gustavo-Paredes <info.gustavoparedes@gmail.com>
2025-09-15 00:06:59 -0300

    FASE 1

--- nmcollections/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-#Se define la ruta y nombre para las vistas
-urlpatterns = [   
-    path('', views.home, name='index'),
-]
--- nmcollections/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Funcion para renderizar la plantilla home.html
-def home(request):
-        return render(request, 'inicio/index.html')
-
